bf653401b63c762febdfa05b15512d995a0d480c
Narasa101 <narasar198@gmail.com>
2022-11-25 22:03:26 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def virat(request):
+    return HttpResponse('Virat is a Great Player I have Never seen')
